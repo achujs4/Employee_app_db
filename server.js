cdc4f60b307a73f0c2434957b43b4fc6531ef892
ASHWINI J S <ashwini4saj@gmail.com>
2025-01-08 22:31:52 +0530

    message

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 
 
 app.set('view engine', 'ejs');
